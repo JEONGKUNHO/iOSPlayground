2e87bbaf006e522bb82d3151728131b0002b5516
JEONGKUNHO <dghj6739@gmail.com>
2025-07-22 23:43:32 +0900

    create EventKitDemo App

---
+++ Projects/Features/EventKitDemo/Targets/EventKitDemo/Sources/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  EventKitDemo
+//
+//  Created by 정건호 on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
